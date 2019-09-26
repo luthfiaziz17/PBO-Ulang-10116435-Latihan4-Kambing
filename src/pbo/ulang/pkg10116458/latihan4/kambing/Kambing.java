c65234601370189c325dee42a4aeb4582f963229
nanda <nanda@nanda>
2019-09-26 23:05:23 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan4/kambing/Kambing.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan4.kambing;
+
+/**
+ *
+ * @author 
+ * NAMA     : Luthfi Aziz Al Paqih
+ * NIM      : 10116435
+ */
+public class Kambing {
+
+    public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
